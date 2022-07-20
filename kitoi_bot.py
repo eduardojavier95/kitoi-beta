fbb426d40f181e22077a9d57958db0879ba569c6
eduardojavier95 <emp9506@gmail.com>
2022-07-19 22:45:00 -0400

    fix 2

--- kitoi_bot.py
+++ kitoi_bot.py
@@ -6,8 +6,8 @@ import telebot
 from telebot import apihelper
 from telebot.types import InlineKeyboardMarkup  # para crear botonera inline
 from telebot.types import InlineKeyboardButton  # para definir botones inline
-from .config import *
-from .utils import funcion_repeat, generar_botones, get_all_keys, get_btn_and_msg, get_categorias, get_list_categorias, get_list_subcategorias, get_subcategorias
+from configuracion_bot import *
+from utils import funcion_repeat, generar_botones, get_all_keys, get_btn_and_msg, get_categorias, get_list_categorias, get_list_subcategorias, get_subcategorias
 from flask import Flask, request
 from waitress import serve
 
